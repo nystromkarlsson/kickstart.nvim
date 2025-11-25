61505414d357ec042f90af04c645476d81deef69
Alexander Nyström <alexander.nystrom@oneflow.com>
2025-11-25 13:55:51 +0100

    add leap, add oil, remove file_browser

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    dependencies = {
+      {
+        'nvim-mini/mini.icons',
+        opts = {},
+      },
+    },
+    lazy = false,
+  },
+}
